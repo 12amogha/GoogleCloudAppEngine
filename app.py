a015cec0c9a745ab2316ff226361aaa2861ac694
Amogha B V <118527592+12amogha@users.noreply.github.com>
2024-06-18 08:51:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
-@app.route('/another')
-def another_page():
-    return 'This is another page!'
+    return 'Hello, World! This is my static website.'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
